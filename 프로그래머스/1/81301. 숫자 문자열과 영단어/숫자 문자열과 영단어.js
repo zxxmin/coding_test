7f8017de62e364fc62fd53ce47d209d3e53e695a
songjimin <119916493+zxxmin@users.noreply.github.com>
2024-06-19 10:34:27 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,7 +1,7 @@
 const solution = (s) => {
     const eng = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
-    eng.filter((item, idx) => {
+    eng.forEach((item, idx) => {
         for(let i=0; i < s.length; i++) {
             if(s.indexOf(item) !== -1) s = s.replace(item, idx);
         }
