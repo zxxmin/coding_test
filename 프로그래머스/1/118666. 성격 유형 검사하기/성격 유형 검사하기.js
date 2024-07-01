b86bc941969788912d97bb129f96fbbf89bea23b
songjimin <119916493+zxxmin@users.noreply.github.com>
2024-07-01 15:05:08 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.85 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,33 +1,20 @@
 const solution = (survey, choice) => {
     let newsurvey = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
-    survey.map((el, i) => {
-        switch (choice[i]) {
-            case 7 :
-                newsurvey[survey[i][1]] += 3
-                break;
-            case 6 :
-                newsurvey[survey[i][1]] += 2
-                break;
-            case 5 :
-                newsurvey[survey[i][1]] += 1
-                break;
-            case 3 :
-                newsurvey[survey[i][0]] += 1
-                break;
-            case 2 :
-                newsurvey[survey[i][0]] += 2
-                break;
-            case 1 :
-                newsurvey[survey[i][0]] += 3
-                break;
-            default : 
-                break;
-
-        }
+    
+    const totalScore = (type, points) => newsurvey[type] += points;
+    
+    survey.forEach((el, i) => {
+        const [firstType, lastType] = el;
+        const score = choice[i]
+        const points = Math.abs(score - 4);
+        
+        if (score > 4) totalScore(lastType, points)
+        else totalScore(firstType, points);
     })
 
 
-    let answer = newsurvey.R >= newsurvey.T ? "R" : "T";
+    let answer = '';
+    answer += newsurvey.R >= newsurvey.T ? "R" : "T";
     answer += newsurvey.C >= newsurvey.F ? "C" : "F";
     answer += newsurvey.J >= newsurvey.M ? "J" : "M";
     answer += newsurvey.A >= newsurvey.N ? "A" : "N";
