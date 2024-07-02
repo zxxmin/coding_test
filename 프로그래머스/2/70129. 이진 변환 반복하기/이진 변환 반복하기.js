e5ca3658d06da3a5976386327c3fc7fe57518dd4
songjimin <119916493+zxxmin@users.noreply.github.com>
2024-07-02 14:17:46 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.40 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -2,11 +2,9 @@ const solution = (s) => {
     let zero = 0;
     let loop = 0;
     
-    for(let i = 0; i <= s.length; i++) {
-        s = s.split('').map(el => el === '1' ? '1' : (++zero, '')).join('').length.toString(2);
-        loop++;
-        if(s.length === 1 && s === '1') break
-        else i=0;
+    while (s.length > 1) {
+        s = s.split('').map(el => el === '1' ? '1' : (++zero, '')).join('').length.toString(2)
+        loop++
     }
     
     return [loop, zero]
