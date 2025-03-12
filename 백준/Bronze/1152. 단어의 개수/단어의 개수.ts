884f35593103c8aaf3ab9e8e4a2db60f55c5d310
songjimin <119916493+zxxmin@users.noreply.github.com>
2025-03-12 10:35:05 +0900

    [Bronze II] Title: 단어의 개수, Time: 192 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.ts
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.ts
@@ -1,2 +1,2 @@
 const input: string = require("fs").readFileSync("/dev/stdin", "utf8").trim();
-console.log(input.split(/\s+/).filter(w => w !== "").length)
+console.log(input.split(' ').filter(w => w !== "").length)
